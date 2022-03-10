8a8052a937d43320e04cee0d90e6f0ff8b7d5b63
IskahnX <davidecroce10@gmail.com>
2022-03-10 17:07:45 +0100

    select all math department teachers

--- file2.sql
+++ file2.sql
@@ -26,3 +26,13 @@ JOIN `courses` ON `degrees` . `id` = `courses` . `degree_id`
 JOIN `course_teacher` ON `course_teacher` . `course_id` = `courses` . `id`
 JOIN `teachers` ON `teachers` . `id` = `course_teacher` . `teacher_id`
 ORDER BY `degrees` . `name`
+
+-- SELEZIONARE TUTTI I DOCENTI DEL DIPARTIMENTO DI MATEMATICA
+
+SELECT `teachers` . `name` AS `Nome`, `teachers` . `surname` AS `Cognome` 
+FROM `teachers` 
+JOIN `course_teacher` ON `course_teacher` . `teacher_id` = `teachers` . `id`
+JOIN `courses` ON `courses` . `id` = `course_teacher` . `course_id`
+JOIN `degrees` ON `degrees` . `id` = `courses` . `degree_id`
+JOIN `departments` ON `departments` . `id` = `degrees` . `department_id`
+WHERE `departments` . `name` = 'Dipartimento di Matematica'
