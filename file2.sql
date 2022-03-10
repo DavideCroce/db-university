3b9aa88e499085c1bf920cc7d20461bba035c010
IskahnX <davidecroce10@gmail.com>
2022-03-10 15:38:15 +0100

    degrees on neuroscience departement

--- file2.sql
+++ file2.sql
@@ -2,3 +2,6 @@
 
 SELECT * FROM `students` JOIN `degrees` ON `degrees` . `id` = `students` . `degree_id` WHERE `degree_id` = 53
 
+-- SELEZIONARE TUTTI I CORSI DI LAUREA DEL DIPARTIMENTO DI NEUROSCIENZE
+
+SELECT * FROM `degrees` JOIN `departments` ON `departments` . `id` = `degrees` . `department_id` WHERE `department_id` = 7
