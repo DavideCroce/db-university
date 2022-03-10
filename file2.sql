3209ecf80f1716237be90b1f5d3186cde435e2f7
IskahnX <davidecroce10@gmail.com>
2022-03-10 16:47:28 +0100

    select all students on their degrees

--- file2.sql
+++ file2.sql
@@ -9,3 +9,11 @@ SELECT * FROM `degrees` JOIN `departments` ON `departments` . `id` = `degrees` .
 -- SELEZIONARE TUTTI I CORSI IN CUI INSEGNA FULVIO AMATO
 
 SELECT * FROM `courses` JOIN `course_teacher` ON `course_teacher` . `course_id` = `courses` . `id` JOIN `teachers` ON `course_teacher` . `teacher_id` = `teachers` . `id` WHERE `teacher_id` = 44
+
+-- SELEZIONARE TUTTI GLI STUDENTI CON I DATI RELATIVI AL CORSO E IL RELATIVO DIPARTIMENTO, IN ORDINE ALFABETICO PER COGNOME E NOME
+
+SELECT `students` . `name`, `students` . `surname`, `degrees` . `name`, `degrees` . `level`, `degrees` . `address`, `degrees` . `email` , `degrees` . `website`, `departments` . `name`
+FROM `students` 
+JOIN `degrees` ON `degrees` . `id` = `students` . `degree_id`
+JOIN `departments` ON `departments` . `id` = `degrees` . `department_id`
+ORDER BY `students` . `surname` ASC
