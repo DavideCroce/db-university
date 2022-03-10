1e62a93a1feb091f70698fe120199f3d1e63a46e
IskahnX <davidecroce10@gmail.com>
2022-03-10 16:11:23 +0100

    select all courses of fulvio amato

--- file2.sql
+++ file2.sql
@@ -5,3 +5,7 @@ SELECT * FROM `students` JOIN `degrees` ON `degrees` . `id` = `students` . `degr
 -- SELEZIONARE TUTTI I CORSI DI LAUREA DEL DIPARTIMENTO DI NEUROSCIENZE
 
 SELECT * FROM `degrees` JOIN `departments` ON `departments` . `id` = `degrees` . `department_id` WHERE `department_id` = 7
+
+-- SELEZIONARE TUTTI I CORSI IN CUI INSEGNA FULVIO AMATO
+
+SELECT * FROM `courses` JOIN `course_teacher` ON `course_teacher` . `course_id` = `courses` . `id` JOIN `teachers` ON `course_teacher` . `teacher_id` = `teachers` . `id` WHERE `teacher_id` = 44
